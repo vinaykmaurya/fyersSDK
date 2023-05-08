edcbfd0ffee010d6824ea439b4fc2fec78d1247a
vinaykmaurya <vinay.maurya@fyers.in>
2023-05-08 16:01:07 +0530

    websocket logger added

--- ws.py
+++ ws.py
@@ -1,15 +1,27 @@
 import asyncio
-import threading
+import logging
+from logging.config import dictConfig
+import os
+import sys
 import time
 import websockets
 import json
-import asyncio
 
 
+class FyersSocket:
+    """
+    A class for sending and receiving data via WebSocket for orders, trades, and positions.
 
-class websocket:
-    def __init__(self, access_token,data_type): 
+    """
+
+    def __init__(self, access_token,data_type,log_path=None): 
         self.access_token = access_token
+        self.log_path = log_path
+        self.background_flag = True
+        self.logger_setup()
+        self.logger.info("Initiate socket object")
+        self.logger.debug('access_token ' + self.access_token)
+        self.log_path =  os.path.join(self.log_path, 'fyers_socket.log')
 
         self.socket_type = {
                "orderUpdate" : "trades,positions,orders",
@@ -21,69 +33,84 @@ class websocket:
         self.data_type = [self.socket_type[(type)] for type in data_type.split(",")]
 
     def parse_position_data(self, msg):
-        position_data_keys = ["buy_avg", "buy_qty", "buy_val", "cf_buy_qty", "cf_sell_qty", "day_buy_qty", "day_sell_qty", "fy_token", "id", "net_avg", "net_qty", "pl_realized", "product_type", "segment", "sell_avg", "sell_qty", "sell_val", "symbol", "tran_side"]
-        position_data = {key: msg['positions'][key] for key in position_data_keys}
-        position_data.update({
-            "qty_multi": msg['positions']['qty_multiplier'],
-            "rbi_ref_rate": msg['positions']['rbirefrate'],
-            "sym_desc": msg['positions']['symbol_desc']
-        })
+        try:
+            position_data_keys = ["buy_avg", "buy_qty", "buy_val", "cf_buy_qty", "cf_sell_qty", "day_buy_qty", "day_sell_qty", "fy_token", "id", "net_avg",
+                                   "net_qty", "pl_realized", "product_type", "segment", "sell_avg", "sell_qty", "sell_val", "symbol", "tran_side"]
+            position_data = {key: msg['positions'][key] for key in position_data_keys}
+            position_data.update({
+                "qty_multi": msg['positions']['qty_multiplier'],
+                "rbi_ref_rate": msg['positions']['rbirefrate'],
+                "sym_desc": msg['positions']['symbol_desc']
+            })
 
-        return {
-            "ws_type": 1,
-            "s": msg["s"],
-            "d": position_data,
-        }
+            return {
+                "ws_type": 1,
+                "s": msg["s"],
+                "d": position_data,
+            }
+        except Exception as e:
+            print(e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logging.error("payload_creation :: ERR : -> Line:{} Exception:{}".format(exc_tb.tb_lineno, str(e)))
 
     def parse_trade_data(self, msg):
-        trade_data_keys = ["id_fill", "id", "qty_traded", "price_traded", "traded_val", "product_type", "client_id", "id_exchange", "ord_type", "tran_side", "symbol", "time_epoch", "fy_token"]
-        trade_data = dict((key, msg['trades'][key]) for key in trade_data_keys)
-        trade_data['tradeNumber'] = msg['trades']['id']
+        try:
+            trade_data_keys = ["id_fill", "id", "qty_traded", "price_traded", "traded_val", "product_type", "client_id", "id_exchange", "ord_type",
+                                "tran_side", "symbol", "time_epoch", "fy_token"]
+            trade_data = dict((key, msg['trades'][key]) for key in trade_data_keys)
+            trade_data['tradeNumber'] = msg['trades']['id']
 
-        return {
-            "ws_type": 1,
-            "s": msg["s"],
-            "d": trade_data,
-        }
+            return {
+                "ws_type": 1,
+                "s": msg["s"],
+                "d": trade_data,
+            }
+        except Exception as e:
+            print(e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logging.error("payload_creation :: ERR : -> Line:{} Exception:{}".format(exc_tb.tb_lineno, str(e)))
     
     def parse_orderUpdate_data(self, msg):
-        keyMap = {
-            "update_time_epoch_oms": "orderDateTime",
-            "id": "exchOrdId",
-            "product_type": "productType",
-            "instrument": "instrument",
-            "side": "side",
-            "ord_status": "status",
-            "qty": "qty",
-            "qty_filled": "filledQty",
-            "qty_remaining": "remainingQuantity",
-            "ord_type": "type",
-            "validity": "orderValidity",
-            "offline_flag": "offlineOrder",
-            "status_msg": "message",
-            "symbol": "symbol",
-            "fy_token": "fyToken",
-            "segment": "segment",
-            "dqQtyRem": "dqQtyRem",
-            "price_limit": "limitPrice",
-            "price_stop": "stopPrice",
-            "qty_disc": "discloseQty",
-            "price_traded": "tradedPrice",
-        }
+        try:
+            keyMap = {
+                "update_time_epoch_oms": "orderDateTime",
+                "id": "exchOrdId",
+                "product_type": "productType",
+                "instrument": "instrument",
+                "side": "side",
+                "ord_status": "status",
+                "qty": "qty",
+                "qty_filled": "filledQty",
+                "qty_remaining": "remainingQuantity",
+                "ord_type": "type",
+                "validity": "orderValidity",
+                "offline_flag": "offlineOrder",
+                "status_msg": "message",
+                "symbol": "symbol",
+                "fy_token": "fyToken",
+                "segment": "segment",
+                "dqQtyRem": "dqQtyRem",
+                "price_limit": "limitPrice",
+                "price_stop": "stopPrice",
+                "qty_disc": "discloseQty",
+                "price_traded": "tradedPrice",
+            }
+            order = msg["orders"]
+            order_data = {keyMap[key]: order.get(key, 0) for key in keyMap}
 
-        order = msg["orders"]
-        order_data = {keyMap[key]: order.get(key, 0) for key in keyMap}
+            order_data["orderNumStatus"] = order["id"] + ":" + str(order["org_ord_status"])
+            order_data["id"] = order["id"]
+            order_data["slNo"] = int(time.time())
 
-        order_data["orderNumStatus"] = order["id"] + ":" + str(order["org_ord_status"])
-        order_data["id"] = order["id"]
-        order_data["slNo"] = int(time.time())
-
-        return {
-            "ws_type": 1,
-            "s": msg["s"],
-            "d": order_data,
-        }
-
+            return {
+                "ws_type": 1,
+                "s": msg["s"],
+                "d": order_data,
+            }
+        except Exception as e:
+            print(e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            logging.error("payload_creation :: ERR : -> Line:{} Exception:{}".format(exc_tb.tb_lineno, str(e)))
 
     async def subscribe(self):
         async with websockets.connect(
@@ -98,13 +125,14 @@ class websocket:
                 # print(f"Received response: {response}")
                 response = json.loads(response)
                 if "orders" in response:
-                    print(self.parse_orderUpdate_data(response))
+                    print(f"Received response: {self.parse_orderUpdate_data(response)}")
                 elif "positions" in response:
-                    print(self.parse_position_data(response))
+                    print(f"Received response: {self.parse_position_data(response)}")
                 elif "trades" in response:
-                    print(self.parse_trade_data(response))
+                    print(f"Received response: {self.parse_trade_data(response)}")
                 else:
-                    print(response)
+                    print(f"Received response: {response}")
+                self.logger.debug(f"Response:{response}")
 
     async def close(self, websocket_task):
         if not websocket_task.done():
@@ -129,9 +157,46 @@ class websocket:
             loop.close()
 
 
+    def logger_setup(self):
+        if self.log_path is None:
+            self.log_path = os.getcwd()
+        os.chmod(self.log_path, 0o750)
+        LOGGING = {
+			'version': 1,
+			'disable_existing_loggers': False,
+			'formatters': {
+				'verbose': {
+					'format': '%(asctime)s - %(name)s - %(levelname)s - %(pathname)s - %(lineno)d - %(message)s'
+				}
+			},
+			'handlers': {
+				'console': {
+					'class': 'logging.StreamHandler',
+					'level': 'DEBUG',
+					'formatter': 'verbose',
+				},
+				'file': {
+					'class': 'logging.FileHandler',
+					'level': 'DEBUG',
+					'formatter': 'verbose',
+					'filename': os.path.join(self.log_path, 'fyers_socket.log')
+				}
+			},
+			'loggers': {
+				'fyers_socket': {
+					'handlers': ['file'],
+					'level': 'DEBUG',
+					'propagate': False,
+				}
+			},
+		}
+        dictConfig(LOGGING)
+        self.logger = logging.getLogger('fyers_socket')
+
+
 client_id = "XC4EOD67IM-100"
 access_token=  "XC4EOD67IM-100:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE2ODM1MTg1NDksImV4cCI6MTY4MzU5MjIyOSwibmJmIjoxNjgzNTE4NTQ5LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCa1dIUlZQalBZYnpWODFfM1BJMHNXN1g1TXhDU0R3VjRsb29WZFhudUNGUHlqY2VwdDZwYkdDeFBRN0JYY2ZDdVpOT0xMSGRjN2R3emdFa0JJSXc1QjdHMFNIUWZjbFF4Y2J3OXJjc2ZhQm9PbTVzWT0iLCJkaXNwbGF5X25hbWUiOiJWSU5BWSBLVU1BUiBNQVVSWUEiLCJvbXMiOiJLMSIsImZ5X2lkIjoiWFYyMDk4NiIsImFwcFR5cGUiOjEwMCwicG9hX2ZsYWciOiJOIn0.7r_NPh7GdNt7iudLwISMQAEhHksowkxuEHlG8-76mzo"
 
-fyers= websocket(access_token,"OnOrders,OnTrades,OnPositions")
+fyers= FyersSocket(access_token,"OnOrders,OnTrades,OnPositions",None)
 fyers.main()
 
